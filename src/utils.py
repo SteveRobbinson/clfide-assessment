1cf1cfb5a8846dfb64bb0eee51f3280be48f1ce3
Luki <lukasjagusz6@gmail.com>
2026-08-04 15:37:48 +0200

    refactor: remove valiation logic from parse_role_expression, utilize sets

--- src/utils.py
+++ src/utils.py
@@ -1,28 +1,14 @@
 import pandas as pd
-from exceptions import InvalidMathExpressionError
-from validators import validate_column_name
 import operator
 from typing import Callable
+import re
 
-def parse_role_expression(role: str, allowed_operators: str = '+-*') -> tuple[list[str], str]:
-    
-    if not isinstance(role, str):
-        raise InvalidMathExpressionError(details=f"Expected str as role, got {type(role).__name__}")
+def parse_role_expression(role: str, allowed_operators: str = '+-*') -> tuple[list[str], set[str]]:
 
-    operator = [znak for znak in role if znak in allowed_operators] 
-    if len(operator) != 1:
-        raise InvalidMathExpressionError(details=f"Expected 1 allowed operator (+, -, *), got {len(operator)}")
+    operators = set(allowed_operators).intersection(role)
+    parsed_expression = [x.strip() for x in re.split(f"([{''.join(operators)}])", role) if x and x.strip()]
 
-    lista = role.split(operator[0])
-    lista = [x.strip() for x in lista]
-
-    for x in lista:
-        if len(x) == 0:
-            raise InvalidMathExpressionError()
-
-    validate_column_name(lista)
-
-    return lista, operator[0]
+    return parsed_expression, operators
 
 
 def compute_new_column(df: pd.DataFrame,

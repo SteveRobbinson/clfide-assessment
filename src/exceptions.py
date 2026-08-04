b42fd6a4ff0ed810fa61e9fe761e97fc9ff4f956
Luki <lukasjagusz6@gmail.com>
2026-08-04 12:20:29 +0200

    refactor: pass arguments to parent class

--- src/exceptions.py
+++ src/exceptions.py
@@ -9,25 +9,11 @@ class ValidationError(Exception):
 
 
 class InvalidMathExpressionError(ValidationError):
-    def __init__(self, message: str | None = None):
-        base_msg = "Given math expression is invalid!"
-        if message:
-            super().__init__(f"{base_msg}\n{message}")
-        else:
-            super().__init__(f"{base_msg}")
+    def __init__(self, base_message: str = "Given math expression is invalid!", details: str | None = None):
+        super().__init__(base_message, details)
+
 
 class InvalidColumnNameError(ValidationError):
-    def __init__(self, message: str | None = None):
-        base_msg = "Input column name is incorrect! Allowed characters: _ and (a-z)"
-        if message:
-            super().__init__(f"{base_msg}\n{message}")
-        else:
-            super().__init__(f"{base_msg}")
+    def __init__(self, base_message: str = "Input column name is incorrect! Allowed characters: (a-z) and (_).", details: str | None = None):
+            super().__init__(base_message, details)
 
-class InvalidColumnTypeError(ValidationError):
-    def __init__(self, message: str | None = None):
-        base_msg = "Column type mismatch: numeric type expected"
-        if message:
-            super().__init__(f"{base_msg}\n{message}")
-        else:
-            super().__init__(base_msg)

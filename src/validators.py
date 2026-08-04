f5646e8d0f5c1b6bbe9981379f0241d5eec8ffba
Luki <lukasjagusz6@gmail.com>
2026-08-04 15:33:24 +0200

    refactor: simplify column type checks logic

--- src/validators.py
+++ src/validators.py
@@ -9,15 +9,6 @@ def has_valid_column_name(column_name: str) -> bool:
     pattern = re.compile("^[a-z_]+$")
     return bool(pattern.fullmatch(column_name.strip()))
 
-    broken = [x.strip() for x in column_names if not bool(pattern.fullmatch(x.strip()))]
 
-    if broken:
-        raise InvalidColumnNameError(details=f"These columns are invalid: {broken}")
-
-
-def validate_numeric_columns(df: pd.DataFrame, columns: list[str]) -> None:
-
-    non_numeric = [col for col in columns if not pd.api.types.is_numeric_dtype(df[col])]
-    
-    if non_numeric:
-        raise InvalidColumnTypeError(details=f"The following columns are not numeric: {non_numeric}")
+def are_columns_numeric_type(df: pd.DataFrame, columns: list[str]) -> bool:
+    return pd.api.types.is_numeric_dtype(df[columns].values)

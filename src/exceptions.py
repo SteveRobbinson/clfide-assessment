c16df9dc06141e0dd280ab7a6dbf72b551fb93b3
Luki <lukasjagusz6@gmail.com>
2026-08-04 12:14:48 +0200

    feat: implement initialization logic for ValidationError

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,5 +1,12 @@
 class ValidationError(Exception):
-    pass
+    def __init__(self, base_message: str, details: str | None = None):
+        self.base_message = base_message
+        self.details = details
+
+        error_message = f"{base_message}\n{details}" if details else base_message
+
+        super().__init__(error_message)
+
 
 class InvalidMathExpressionError(ValidationError):
     def __init__(self, message: str | None = None):

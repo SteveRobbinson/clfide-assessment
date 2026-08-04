d6511210c7eee2c0b9784629e7ed3ba3466f8089
Luki <lukasjagusz6@gmail.com>
2026-08-04 15:44:59 +0200

    refactor: rename compute_new_column to add_calculated_column

--- src/utils.py
+++ src/utils.py
@@ -11,18 +11,17 @@ def parse_role_expression(role: str, allowed_operators: str = '+-*') -> tuple[li
     return parsed_expression, operators
 
 
-def compute_new_column(df: pd.DataFrame,
-                       new_column_name: str,
-                       column_names: list[str],
-                       math_operator: str,
-                       allowed_operators: dict[str, Callable] = {'+': operator.add,
-                                                                 '-': operator.sub,
-                                                                 '*': operator.mul}
-                      ) -> pd.DataFrame:
+def add_calculated_column(df: pd.DataFrame,
+                          new_column_name: str,
+                          column_names: list[str],
+                          math_operator: str,
+                          allowed_operators: dict[str, Callable] = {'+': operator.add,
+                                                                    '-': operator.sub,
+                                                                    '*': operator.mul}
+                          ) -> pd.DataFrame:
     
     func = allowed_operators[math_operator]
     df_new = df.copy()
-    
     df_new[new_column_name] = func(df_new[column_names[0]], df_new[column_names[1]])
-    
+
     return df_new
